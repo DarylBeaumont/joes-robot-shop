943ab32fab3e63d178f0dc758d680348e889f10d
DarylBeaumont <darylbeaumont@gmail.com>
2024-01-16 22:24:28 +0000

    Added template-driven form
    Added a template driven form for sign-ins. Added validation to fields involved.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './template-form-controls/template-form-controls.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Joe's Robot Shop" },
   { path: 'catalogue', component: CatalogueComponent, title: "Catalogue - Joe's Robot Shop" },
   { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'form-controls', component: TemplateFormControlsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
